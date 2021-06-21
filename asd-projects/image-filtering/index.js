3cb1e7fbf83bcffe67f00f0e8152c882a6b17176
Con-Co-Industries <connor.d.ettinger@gmail.com>
2021-06-21 15:49:56 -0500

    saving image filtering

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -1,9 +1,11 @@
 // This is a small program. There are only two sections. This first section is what runs
 // as soon as the page loads and is where you should call your functions.
 $(document).ready(function(){
+    debugger;
     const $display = $('#display');
 
     // TODO: Call your apply function(s) here
+    applyFilter(reddify);
 
 
 
@@ -17,12 +19,39 @@ $(document).ready(function(){
 /////////////////////////////////////////////////////////
 
 // TODO 1 & 3: Create the applyFilter function here
+function applyFilter(filterFunction){
+    for (var r = 0; r < image.length; r++) { 
+        var row = image[r];
+        
+        for (var c = 0; c < row.length; c++) { 
+            var value = image[r][c];
+            var rgbString = value;
+            var rgbNumbers = rgbStringToArray(rgbString);
 
+            rgbNumbers[RED] = 255;
+            filterFunction(rbgNumbers);
+
+            rgbString = rgbArrayToString(rgbNumbers);
+            image[r][c] = rgbString;
+            
+            //action(value);
+            // or
+            //action(image[r][c])
+            // or;
+            //action(row[c])
+        }
+    }
+        
+}
 
 // TODO 5: Create the applyFilterNoBackground function
 
 
 // TODO 2 & 4: Create filter functions
+
+function reddify(Arr){
+    Arr[RED] = 255;
+}
 
 
 // CHALLENGE code goes below here
