0d66d0f30e79ffc5c55ab0133b881ce959b816ac
Con-Co-Industries <connor.d.ettinger@gmail.com>
2021-06-29 14:48:11 -0500

    saving sorting exercise

--- asd-projects/sorting-exercise/index.js
+++ asd-projects/sorting-exercise/index.js
@@ -14,7 +14,19 @@ The CSS ids you will work with are:
 ///////////////////////////////////////////////////////////////////////
 
 // TODO 2: Implement bubbleSort
+async function bubbleSort(array){
+    for (var i = 0; i < array.length-1; i++){
+        for( var j = i+1; j<array.length-1; j++){
+            if (array[i].value > array[j].value){
+                swap(array, i, j);
+                updateCounter(bubbleCounter);
+                await sleep();
 
+            }
+        }
+    }
+      
+}
 
 // TODO 3: Implement quickSort
 
@@ -23,6 +35,11 @@ The CSS ids you will work with are:
 
 
 // TODO 1: Implement swap
+function swap(array,i,j){
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+}
 
 
 ///////////////////////////////////////////////////////////////////////
